70aa3025ae185f5a8ce3743fa62c4df689468506
sthigabout <hkdt.10@gmail.com>
2018-12-03 00:41:47 +0900

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require('http');
 http
   .createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello World!!\n');
   })
-  .listen(process.env.PORT);
+  .listen(process.env.PORT || 8000);
