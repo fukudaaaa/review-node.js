39b932c408fb74a63e89c34a6421fa7a240750cb
sthigabout <hkdt.10@gmail.com>
2018-12-03 16:35:54 +0900

    test

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 const PORT = process.env.PORT || 8000;
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
