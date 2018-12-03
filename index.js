5cab3cc14ebf4289c53411d881fddfad657dfe37
sthigabout <hkdt.10@gmail.com>
2018-12-03 16:47:53 +0900

    test 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 app
-  // .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
