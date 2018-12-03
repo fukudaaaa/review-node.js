4a77fbdf6e44b5535c137d1e3ec781b93b7c7ddb
sthigabout <hkdt.10@gmail.com>
2018-12-03 12:19:33 +0900

    plain node app

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 8000;
+const http = require('http');
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
